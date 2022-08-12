4926881fac0245ecd6066f7ed7214c3fde5a9d73
Kondratenko Konstia <evpatyikalovrat@gmail.com>
2022-08-12 21:18:32 +0200

    coplete day8 tasks

--- src/main/java/day8/Task1.java
+++ src/main/java/day8/Task1.java
@@ -2,6 +2,29 @@ package day8;
 
 public class Task1 {
     public static void main(String[] args) {
+        final int howManyTimes = 20000;
+        long timeStart = System.currentTimeMillis();
+        String someString = "";
+        for(int i = 0;i<=howManyTimes;i++){
+            someString += i + " ";
+
+        }
+        long timeEnd = System.currentTimeMillis();
+        System.out.println("Time String " + (timeEnd - timeStart));
+        System.out.println(someString);
+
+
+        long timeStartStrBl = System.currentTimeMillis();
+
+        StringBuilder anotherString = new StringBuilder("");
+        for(int i = 0;i<=howManyTimes;i++){
+            anotherString.append(i).append(" ");
+
+        }
+        long timeEndStrBl = System.currentTimeMillis();
+        System.out.println(anotherString);
+        System.out.println("Time SrtBuilder " + (timeEndStrBl - timeStartStrBl) + "ms");
+
 
     }
 }
--- src/main/java/day8/Task2.java
+++ src/main/java/day8/Task2.java
@@ -2,6 +2,8 @@ package day8;
 
 public class Task2 {
     public static void main(String[] args) {
+    Airplane plane = new Airplane("Kub",2001,300,8234);
 
+        System.out.println(plane);
     }
 }
